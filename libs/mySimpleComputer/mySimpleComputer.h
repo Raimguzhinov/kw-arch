9e2e36c1a2bba8f66cce2332ee498d3c76b8e123
Raimguzhinov <d.raimguzhinov@stud.sibguti.ru>
2023-03-14 03:39:17 +0700

    Оставлен только нужный список команд

--- libs/mySimpleComputer/mySimpleComputer.h
+++ libs/mySimpleComputer/mySimpleComputer.h
@@ -25,45 +25,12 @@
 #define JZ 42
 #define HALT 43
 #define NOT 51
-#define AND 52
-#define OR 53
-#define XOR 54
-#define JNS 55
-#define JC 56
-#define JNC 57
-#define JP 58
-#define JNP 59
-#define CHL 60
-#define SHR 61
-#define RCL 62
-#define RCR 63
-#define NEG 64
-#define ADDC 65
-#define SUBC 66
-#define LOGLC 67
-#define LOGRC 68
-#define RCCL 69
-#define RCCR 70
-#define MOVA 71
-#define MOVR 72
-#define MOVCA 73
-#define MOVCR 74
-#define pADDC 75
-#define pSUBC 76
 
 // Макрос для проверки существующей команды процессору
 #define CHECK_CPU_CMD(x)                                                      \
   ((x == READ) || (x == WRITE) || (x == LOAD) || (x == STORE) || (x == ADD)   \
    || (x == SUB) || (x == DIVIDE) || (x == MUL) || (x == JUMP) || (x == JNEG) \
-   || (x == JZ) || (x == HALT) || (x == NOT) || (x == AND) || (x == OR)       \
-   || (x == XOR) || (x == JNS) || (x == JC) || (x == JNC) || (x == JP)        \
-   || (x == JNP) || (x == CHL) || (x == SHR) || (x == RCL) || (x == RCR)      \
-   || (x == NEG) || (x == ADDC) || (x == SUBC) || (x == LOGLC)                \
-   || (x == LOGRC) || (x == RCCL) || (x == RCCR) || (x == MOVA)               \
-   || (x == MOVR) || (x == MOVCA) || (x == MOVCR) || (x == pADDC)             \
-   || (x == pSUBC))                                                           \
-      ? 1                                                                     \
-      : 0
+   || (x == JZ) || (x == HALT) || (x == NOT)) ? 1 : 0
 
 int sc_memoryInit ();
 int sc_memorySet (int address, int value);
