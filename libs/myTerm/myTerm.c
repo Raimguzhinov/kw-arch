fffc545bbe78f457b7a18c3c1dd6acbe3c3a0e6c
Raimguzhinov <d.raimguzhinov@stud.sibguti.ru>
2023-03-14 03:40:11 +0700

    Добавлен функционал для UI терминала

--- libs/myTerm/myTerm.c
+++ libs/myTerm/myTerm.c
@@ -1,96 +1,71 @@
 #include "myTerm.h"
 #include <stdio.h>
 
-int mt_clrscr() {
-    printf("\033[2J");
-    printf("\033[H");
-    return 0;
+int
+mt_clrscr ()
+{
+  printf ("\E[H\E[2J");
+  return 0;
 }
 
-int mt_gotoXY(int x, int y) {
-    printf("\033[%d;%dH", x, y);
-    return 0;
+int
+mt_gotoXY (int x, int y)
+{
+  int count_rows, count_columns;
+
+  if (mt_getscreensize (&count_rows, &count_columns) == -1)
+    {
+      return -1;
+    }
+
+  if ((x > count_columns) || (x <= 0) || (y > count_rows) || (y <= 0))
+    {
+      return -1;
+    }
+
+  printf ("\033[%d;%dH", y, x);
+
+  return 0;
 }
 
-int mt_getscreensize(int* rows, int* cols) {
-    struct winsize w;
-    if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &w) < 0)
-        return -1;
-    *rows = w.ws_row;
-    *cols = w.ws_col;
-    return 0;
+int
+mt_getscreensize (int *rows, int *columns)
+{
+  struct winsize window_size;
+
+  if (ioctl (1, TIOCGWINSZ, &window_size))
+    {
+      return -1;
+    }
+
+  *rows = window_size.ws_row;
+  *columns = window_size.ws_col;
+
+  return 0;
 }
 
-int mt_setfgcolor(enum colors color) {
-    printf("\033[%dm", color);
-    return 0;
+int
+mt_setfgcolor (enum colors color)
+{
+  printf ("\033[38;5;%dm", color);
+  return 0;
 }
 
-int mt_setbgcolor(enum colors color) {
-    printf("\033[%dm", color + 10);
-    return 0;
+int
+mt_setbgcolor (enum colors color)
+{
+  printf ("\033[48;5;%dm", color);
+  return 0;
 }
 
-int main() {
-    mt_clrscr();
-    int rows = 0, cols = 0;
-    mt_getscreensize(&rows, &cols);
-    mt_setbgcolor(BLACK);
-    putchar(' ');
-    mt_setbgcolor(PURPLE);
-    putchar(' ');
+int mt_setdfcolor()
+{
+  printf("\033[0m");
+  return 0;
+}
 
-    mt_setfgcolor(YELLOW);
-    mt_gotoXY(1, 1);
-    printf(" Memory     ");
-
-    mt_setfgcolor(WHITE);
-    mt_gotoXY(2, 2);
-    printf(" Value       ");
-
-    mt_setfgcolor(YELLOW);
-    mt_gotoXY(3, 1);
-    printf(" AX  ");
-    mt_gotoXY(4, 1);
-    printf(" BX  ");
-    mt_gotoXY(5, 1);
-    printf(" CX  ");
-    mt_gotoXY(6, 1);
-    printf(" DX  ");
-
-    mt_gotoXY(3, 15);
-    printf("0x00  ");
-    mt_gotoXY(4, 15);
-    printf("0x00  ");
-    mt_gotoXY(5, 15);
-    printf("0x00  ");
-    mt_gotoXY(6, 15);
-    printf("0x00  ");
-
-    mt_gotoXY(8, 1);
-    printf("Function keys:");
-
-    mt_gotoXY(9, 1);
-    printf("F1  ");
-    mt_gotoXY(10, 1);
-    printf("F2  ");
-    mt_gotoXY(11, 1);
-    printf("F3  ");
-    mt_gotoXY(12, 1);
-    printf("F4  ");
-    mt_gotoXY(13, 1);
-    printf("F5  ");
-
-    mt_gotoXY(9, 15);
-    printf("Load  ");
-    mt_gotoXY(10, 15);
-    printf("Save  ");
-    mt_gotoXY(11, 15);
-    printf("Run   ");
-    mt_gotoXY(12, 15);
-    printf("Step  ");
-    mt_gotoXY(13, 15);
-    printf("Reset ");
-
-    return 0;
+int mt_printsymbol(char symbol)
+{
+  printf ("\033(0%c\033(B", symbol);
+  return 0;
 }
--- libs/myTerm/myTerm.h
+++ libs/myTerm/myTerm.h
@@ -3,10 +3,21 @@
 #include <termios.h>
 #include <sys/ioctl.h>
 
-enum colors {BLACK = 30, RED, GREEN, YELLOW, BLUE, PURPLE, CYAN, WHITE};
+#define UL_CORNER 'l' // левый верхний угол
+#define UR_CORNER 'k' // правый верхний угол
+#define DR_CORNER 'j' // правый нижний угол
+#define DL_CORNER 'm' // левый нижний угол
+#define H_LINE 'q'    // горизонтальная линия
+#define V_LINE 'x'    // вертикальная линия
+#define FILL 'a'      // штриховка
 
-int mt_clrscr();
+enum colors {WHITE = 15, BLACK, RED = 196, BLUE = 30, YELLOW, GREEN = 34, PURPLE, CYAN = 36};
+
+int instruction_counter;
+int mt_clrscr(void);
 int mt_gotoXY(int x, int y);
 int mt_getscreensize(int* rows, int* cols);
 int mt_setfgcolor(enum colors color);
 int mt_setbgcolor(enum colors color);
+int mt_setdfcolor(void);
+int mt_printsymbol(char symbol);
