62d06a19412de5f7cabd0d4a540f33f4c1ba494b
Raimguzhinov <d.raimguzhinov@stud.sibguti.ru>
2023-04-11 05:36:22 +0700

    Изменён вывод в файловый дескриптор

--- libs/myTerm/myTerm.h
+++ libs/myTerm/myTerm.h
@@ -7,16 +7,17 @@
 
 extern int instruction_counter;
 
-#define UL_CORNER 'l' // левый верхний угол
-#define UR_CORNER 'k' // правый верхний угол
-#define DR_CORNER 'j' // правый нижний угол
-#define DL_CORNER 'm' // левый нижний угол
-#define H_LINE 'q'    // горизонтальная линия
-#define V_LINE 'x'    // вертикальная линия
-#define FILL 'a'      // штриховка
+#define ACS_CKBOARD 'a'  // штриховка
+#define ACS_ULCORNER 'l' // левый верхний угол
+#define ACS_URCORNER 'k' // правый верхний угол
+#define ACS_LRCORNER 'j' // правый нижний угол
+#define ACS_LLCORNER 'm' // левый нижний угол
+#define ACS_HLINE 'q'    // горизонтальная линия
+#define ACS_VLINE 'x'    // вертикальная линия
 
 enum colors
 {
+  DEFAULT,
   WHITE = 15,
   BLACK,
   RED = 196,
