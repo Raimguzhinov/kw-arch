782e2ddecce16c0c5b1e0dd1d649591c73738550
Raimguzhinov <d.raimguzhinov@stud.sibguti.ru>
2023-04-20 03:27:24 +0700

    Добавлена функция для инициализации шрифта

--- libs/myBigChars/myBigChars.h
+++ libs/myBigChars/myBigChars.h
@@ -1,16 +1,15 @@
 // Copyright [2023] <Raimguzhinov>
 #pragma once
-#include "../myTerm/myTerm.h"
-// #include <stdbool.h>
 #include <fcntl.h>
+#include <myTerm.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#define BC_SIZE_DEFAULT 2
 
 extern int big_chars[][2];
-int BC_SIZE;
+int font[36];
 
 int bc_printA (char str);
 int bc_box (int x1, int y1, int x2, int y2);
@@ -19,3 +18,4 @@ int bc_setbigcharpos (int *big, int x, int y, int value);
 int bc_getbigcharpos (int *big, int x, int y, int *value);
 int bc_bigcharwrite (int fd, int *big, int count);
 int bc_bigcharread (int fd, int *big, int need_count, int *count);
+void bc_initfont (char *filename);
