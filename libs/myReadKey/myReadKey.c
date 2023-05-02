ac71a85ad72c5b97f44e8d5457e046c76fd6ab04
Raimguzhinov <d.raimguzhinov@stud.sibguti.ru>
2023-05-03 03:34:53 +0700

    Исправлено форматирование

--- libs/myReadKey/myReadKey.c
+++ libs/myReadKey/myReadKey.c
@@ -114,18 +114,18 @@ int
 rk_myTermRegime (int regime, int vtime, int vmin, int echo, int sigint)
 {
   struct termios curr;
-  tcgetattr(fileno(stdin), &curr) ;
+  tcgetattr (fileno (stdin), &curr);
 
-  if(regime)
-      curr.c_lflag |= ICANON ;
-  else{
-      curr.c_lflag &= ~ICANON ;
-      sigint ? (curr.c_lflag |= ISIG) : (curr.c_lflag &= ~ISIG) ;
-      echo ? (curr.c_lflag |= ECHO) : (curr.c_lflag &= ~ECHO) ;
-      curr.c_cc[VMIN] = vmin ;
-      curr.c_cc[VTIME] = vtime ;
-
-  }
-  tcsetattr(0,TCSAFLUSH,&curr) ;
-  return 0 ;
+  if (regime)
+    curr.c_lflag |= ICANON;
+  else
+    {
+      curr.c_lflag &= ~ICANON;
+      sigint ? (curr.c_lflag |= ISIG) : (curr.c_lflag &= ~ISIG);
+      echo ? (curr.c_lflag |= ECHO) : (curr.c_lflag &= ~ECHO);
+      curr.c_cc[VMIN] = vmin;
+      curr.c_cc[VTIME] = vtime;
+    }
+  tcsetattr (0, TCSAFLUSH, &curr);
+  return 0;
 }
